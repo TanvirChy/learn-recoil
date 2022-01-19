192f03cef345a5cf251afc3828c6da75d42b8757
Tanvir Chowdhury <50425710+TanvirChy@users.noreply.github.com>
2022-01-19 17:38:45 +0600

    learn atom , selector

---
+++ src/components/CharacterCounter.jsx
@@ -0,0 +1,45 @@
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
+
+const textState = atom({
+  key: "textState",
+  default: "",
+});
+
+export default function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo : {text}
+    </div>
+  );
+}
+
+const charCountState = selector({
+  key: "charCountState",
+  get: ({ get }) => {
+    const text = get(textState);
+    return text.length;
+  },
+});
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <div>CharCounter Count: {count}</div>;
+}
